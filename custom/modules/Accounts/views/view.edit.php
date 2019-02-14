b67e1fa9f80e0216d75d49c2bce3e2f6d1dc61d3
Cloud User <centos@ip-172-31-9-116.ap-southeast-1.compute.internal>
2019-02-14 17:12:31 +0000

    Hide Project Subpanel from customers modules

---
+++ custom/modules/Accounts/views/view.edit.php
@@ -0,0 +1,24 @@
+<?php
+
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+require_once('include/MVC/View/views/view.edit.php');
+
+class LeadsViewEdit extends ViewEdit {
+
+    function LeadsViewEdit(){
+        parent::ViewEdit();
+    }
+    function display()
+ 	{
+		parent::display();
+        echo <<<EOQ
+		<script>
+            $(document).ready(function(){
+                $("whole_subpanel_project").hide();
+            });
+        </script>
+EOQ;
+ 	}
+}
+
