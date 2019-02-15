002653efcf9f3354e7e47cd4be7812c87b5ba6e3
Cloud User <centos@ip-172-31-9-116.ap-southeast-1.compute.internal>
2019-02-15 09:08:02 +0000

    Quick repair for hiding subpanel from accounts module

--- custom/modules/Accounts/Ext/Layoutdefs/layoutdefs.ext.php
+++ custom/modules/Accounts/Ext/Layoutdefs/layoutdefs.ext.php
@@ -26,6 +26,11 @@ $layout_defs["Accounts"]["subpanel_setup"]['accounts_calls_1'] = array (
 );
 
 
+
+/*Remove subpanel*/
+unset($layout_defs['Accounts']['subpanel_setup']['project']);
+
+
 //auto-generated file DO NOT EDIT
 $layout_defs['Accounts']['subpanel_setup']['account_aos_contracts']['override_subpanel_name'] = 'Account_subpanel_account_aos_contracts';
 
