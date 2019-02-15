b33cf9e0767ba0c04f14ee149126d99aca365a5a
Cloud User <centos@ip-172-31-9-116.ap-southeast-1.compute.internal>
2019-02-15 10:45:54 +0000

    Removing subpanel from accounts module

--- custom/Extension/modules/Accounts/Ext/Layoutdefs/custom_remove_subpanel.php
+++ custom/Extension/modules/Accounts/Ext/Layoutdefs/custom_remove_subpanel.php
@@ -1,4 +1,18 @@
 <?php
 
-/*Remove subpanel*/
+/*Custom remove subpanels*/
 unset($layout_defs['Accounts']['subpanel_setup']['project']);
+
+unset($layout_defs['Accounts']['subpanel_setup']['opportunities']);
+
+unset($layout_defs['Accounts']['subpanel_setup']['campaigns']);
+
+unset($layout_defs['Accounts']['subpanel_setup']['accounts']);
+
+unset($layout_defs['Accounts']['subpanel_setup']['account_aos_quotes']);
+
+unset($layout_defs['Accounts']['subpanel_setup']['account_aos_invoices']);
+
+unset($layout_defs['Accounts']['subpanel_setup']['account_aos_contracts']);
+
+unset($layout_defs['Accounts']['subpanel_setup']['products_services_purchased']);
